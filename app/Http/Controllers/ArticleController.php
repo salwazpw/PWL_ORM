5a2abe9cb507ddfab60c13b3f05cc66dfb957e20
salwazpw <salwapw04@gmail.com>
2022-04-18 09:37:38 +0700

    JS10 (Praktikum 2 - Membuat Form Update Berkas)

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Storage;
 class ArticleController extends Controller
 {
     /**
@@ -65,9 +65,11 @@ class ArticleController extends Controller
      * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
      */
-    public function edit(Article $article)
+    public function edit($id)
     {
-        //
+        $article = Article::find($id);
+
+        return view('articles.edit', ['article' => $article]);
     }
 
     /**
@@ -77,9 +79,21 @@ class ArticleController extends Controller
      * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Article $article)
+    public function update(Request $request, $id)
     {
-        //
+        $article = Article::find($id);
+
+        $article->title = $request->title;
+        $article->content = $request->content;
+
+        if($article->featured_image && file_exists(storage_path('app/public/'. $article->featured_image))){
+            \Storage::delete('public/'. $article->featured_image);
+        }
+        $image_name = $request->file('image')->store('images', 'public');
+        $article->featured_image = $image_name;
+
+        $article->save();
+        return 'Artikel berhasil diubah';
     }
 
     /**
